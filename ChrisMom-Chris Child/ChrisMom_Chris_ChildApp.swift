515250953b7cb51a68e3ccd2315ba209c4921fc5
Kesavan Panchabakesan <pkesavan@greatergoods.com>
2022-12-23 21:37:44 +0530

    Initial Commit

---
+++ ChrisMom-Chris Child/ChrisMom_Chris_ChildApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChrisMom_Chris_ChildApp.swift
+//  ChrisMom-Chris Child
+//
+//  Created by Kesavan Panchabakesan on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ChrisMom_Chris_ChildApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
